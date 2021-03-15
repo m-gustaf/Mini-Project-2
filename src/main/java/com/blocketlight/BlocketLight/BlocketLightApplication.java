698f183d41c88d4ed2d149ec2095db891e08e487
Tobias Iderbrandt <tobias.iderbrandt@hm.com>
2021-03-15 16:28:56 +0100

    Added some html pages

--- src/main/java/com/blocketlight/BlocketLight/BlocketLightApplication.java
+++ src/main/java/com/blocketlight/BlocketLight/BlocketLightApplication.java
@@ -2,9 +2,11 @@ package com.blocketlight.BlocketLight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class BlocketLightApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlocketLightApplication.class, args);
--- src/main/java/com/blocketlight/BlocketLight/BlocketLightController.java
+++ src/main/java/com/blocketlight/BlocketLight/BlocketLightController.java
@@ -1,15 +1,18 @@
 package com.blocketlight.BlocketLight;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Controller
+
 public class BlocketLightController {
+
 
     @Autowired
     ItemRepository itemRepository;
@@ -29,5 +32,48 @@ public class BlocketLightController {
         return "detailPage";
     }
 
+    @GetMapping("/listItems")
+    public String listItems(Model model) {
+        List<Item> list = itemRepository.getList();
+        model.addAttribute("items", list);
 
+        return "listItems";
+    }
+
+    @GetMapping("/addItem")
+    public String addItems(Model model) {
+        List<Item> list = itemRepository.getList();
+        model.addAttribute("items", list);
+
+        return "addItem";
+    }
+
+    @PostMapping("/save")
+    public String set(Model model) {
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+
+
+        return "login";
+
+    }
+    @GetMapping("/register")
+    public String register(Model model) {
+
+
+        return "register";
+
+    }
+
+    @GetMapping("/forgot")
+    public String forgot(Model model) {
+
+
+        return "forgot";
+
+    }
 }
