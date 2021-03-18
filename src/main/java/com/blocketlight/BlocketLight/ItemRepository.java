f66cd80db7a7923c99c6d8bd5c9e5c89f49206e1
magsa79 <magnus.gustafsson2@hm.com>
2021-03-18 14:44:41 +0100

    removed search in headers

--- src/main/java/com/blocketlight/BlocketLight/ItemRepository.java
+++ src/main/java/com/blocketlight/BlocketLight/ItemRepository.java
@@ -16,7 +16,7 @@ public class ItemRepository {
         items.add(new Item(2, Category.MOUNTAIN_BIKE, "Canyon Spectral 29", 2021, 24990, true,"https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dwcf1ca722/images/full/full_2021_/2021/full_2021_spectral-cf-29-7_2687_bk-bk_P5.png?sw=1060&sh=1060&sm=fit&sfrm=png"));
         items.add(new Item(3, Category.CYCLOCROSS, "Canyon Inflite CF SL", 2021, 34900, true,"https://media-cdn.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dw7abae634/images/full/full_2021_/2021/full_2021_inflite-cf-sl-7_mc_2656_P5.png"));
         items.add(new Item(4, Category.CHILDRENS_BIKE, "Frog 40 Kids Pedal", 2021, 2990, true,"https://www.billybilslandcycles.co.uk/images/frog-40-red.png"));
-        items.add(new Item(5, Category.ELECTRIC_BIKE, "Crescent Electric", 2021, 19990, true,"https://www.elcykelpunkten.se/wp-content/uploads/2020/02/YEC937__55-1-1080x722.jpg"));
+        items.add(new Item(5, Category.ELECTRIC_BIKE, "Crescent Elina 7-speed", 2021, 19990, true,"https://www.elcykelpunkten.se/wp-content/uploads/2020/02/YEC937__55-1-1080x722.jpg"));
         items.add(new Item(6, Category.ROAD_BIKE, "Cannondale Supersix EVO", 2021, 69900, true,"https://www.henrikssonscykel.se/pub_images/large/C21_C11671M_SuperSix_EVO_Crb_BBQ_PD.png?timestamp=1601914342"));
     }
 
@@ -59,11 +59,10 @@ public class ItemRepository {
                 result.add(item);
             }
         }
-
-        if(result.size() > 0){
+        if (result.size() > 0){
           return result;
         }
-        else{
+        else {
             return null;
         }
     }
