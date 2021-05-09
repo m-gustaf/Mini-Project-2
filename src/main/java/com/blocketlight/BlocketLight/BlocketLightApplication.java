8c2c7f362e9273af464f70e5697c90923235461a
Tobias Iderbrandt <tobias.iderbrandt@hm.com>
2021-05-10 00:13:52 +0200

    Added Thymeleaf layout to improve navigation etc

--- src/main/java/com/blocketlight/BlocketLight/BlocketLightApplication.java
+++ src/main/java/com/blocketlight/BlocketLight/BlocketLightApplication.java
@@ -1,8 +1,10 @@
 package com.blocketlight.BlocketLight;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -12,6 +14,11 @@ public class BlocketLightApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlocketLightApplication.class, args);
+	}
+
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
 	}
 
 }
